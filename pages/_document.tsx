02550cb18e06640946728d8e40c0a5510b15eb4a
LGeoff31 <geoffrey31415@gmail.com>
2023-08-05 14:52:12 -0400

    updated deleted pricing component and put new page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
         {/* Your other head elements (stylesheets, scripts, etc.) go here */}
       </Head>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Footer from "./components/Footer";
 import Homepage from "./components/Homepage";
 import HowItWorks from "./components/HowItWorks";
 import Navbar from "./components/Navbar";
-import Pricing from "./components/Pricing";
+// import Pricing from "./components/Pricing";
 import Questions from "./components/Questions";
 import { Element } from "react-scroll";
 
