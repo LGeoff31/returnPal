1034fe2d25426808b45d7dc1f9cd55eaa71190c3
LGeoff31 <geoffrey31415@gmail.com>
2023-08-08 11:51:38 -0400

    swapped get started and sign in button

--- pages/components/Navbar.tsx
+++ pages/components/Navbar.tsx
@@ -73,24 +73,6 @@ const Navbar = () => {
             </li>
           </ul>
           <div className="navigation-buttons">
-            <a href="/signUp">
-              <Button
-                sx={{
-                  variant: "outlined",
-                  color: "grey",
-                  textTransform: "none",
-                  borderRadius: "2rem",
-                  backgroundColor: "#f5f5f5",
-                  border: "none",
-                  fontSize: "15px",
-                  textAlign: "center",
-                  marginRight: "1rem",
-                }}
-              >
-                <AccountCircleIcon /> &nbsp; Sign in
-              </Button>
-            </a>
-
             <a href="/getstarted">
               <Button
                 sx={{
@@ -110,6 +92,23 @@ const Navbar = () => {
                 className="text-[#0099ff] hover:text-white"
               >
                 Get Started &nbsp; <NearMeIcon />
+              </Button>
+            </a>
+            <a href="/signUp">
+              <Button
+                sx={{
+                  variant: "outlined",
+                  color: "black",
+                  textTransform: "none",
+                  borderRadius: "2rem",
+                  backgroundColor: "#f5f5f5",
+                  border: "none",
+                  fontSize: "15px",
+                  textAlign: "center",
+                  marginRight: "1rem",
+                }}
+              >
+                <AccountCircleIcon /> &nbsp; Sign in
               </Button>
             </a>
           </div>
