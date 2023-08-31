45f7deaa5a7faaef0c6640a3274ba2e46f8a37d9
LGeoff31 <geoffrey31415@gmail.com>
2023-08-31 18:47:34 -0400

    try

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,10 @@ import Footer from "./components/Footer";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      {/* <Head /> */}
       <body>
         <Main />
         <NextScript />
