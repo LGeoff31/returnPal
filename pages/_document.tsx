51f4f38931b2470b8db0e2d26c48d2935e94c72d
LGeoff31 <geoffrey31415@gmail.com>
2023-08-31 18:30:28 -0400

    try

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,22 +2,19 @@ import { Html, Head, Main, NextScript } from "next/document";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { Stack } from "@mui/material";
-// import { ChakraProvider } from "@chakra-ui/react";
+import { WidthFull } from "@mui/icons-material";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
-        {/* Your other head elements (stylesheets, scripts, etc.) go here */}
-      </Head>
-
+      {/* <Head> */}
+      {/* <meta name="viewport" content="width=device-width, initial-scale=1.0" /> */}
+      {/* Your other head elements (stylesheets, scripts, etc.) go here */}
+      <Head />
       <body>
-        <Stack>
-          {/* <ChakraProvider> */}
-          <Main />
-          {/* </ChakraProvider> */}
-        </Stack>
+        {/* <Stack> */}
+        <Main />
+        {/* </Stack> */}
         <NextScript />
       </body>
     </Html>
