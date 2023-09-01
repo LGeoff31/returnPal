b1c279c4b3ff50f6b7d1b8d4efae9ddef5083398
LGeoff31 <geoffrey31415@gmail.com>
2023-09-01 00:27:11 -0400

    more responsive phone

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,13 @@ import Footer from "./components/Footer";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
+      {/* <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+      </Head> */}
+      {/* <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head> */}
+      <Head />
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,10 +7,14 @@ import Navbar from "./components/Navbar";
 import Questions from "./components/Questions";
 import { Element } from "react-scroll";
 import Retailers from "./components/Retailers";
+import Head from "next/head";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
       <Layout>
         <Homepage />
         <HowItWorks />
